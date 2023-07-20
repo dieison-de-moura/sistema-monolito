4bdeff6e8670853c392544396238d18f91675b95
dieison-de-moura <di.eison95@outlook.com>
2023-07-20 17:15:36 -0300

    Criando client-adm

---
+++ src/modules/client-adm/repository/client.model.ts
@@ -0,0 +1,26 @@
+import { Column, Model, PrimaryKey, Table } from "sequelize-typescript";
+
+@Table({
+    tableName: "clients",
+    timestamps: false,
+})
+export class ClientModel extends Model {
+    @PrimaryKey
+    @Column({ allowNull: false })
+    declare public id: string;
+
+    @Column({ allowNull: false })
+    declare public name: string;
+
+    @Column({ allowNull: false })
+    declare public email: string;
+
+    @Column({ allowNull: false })
+    declare public address: string;
+
+    @Column({ allowNull: false })
+    declare public createdAt: Date;
+
+    @Column({ allowNull: false })
+    declare public updatedAt: Date;
+}
